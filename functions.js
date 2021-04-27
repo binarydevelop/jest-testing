2a09c19c89d77ab466d71d5020a5e6e53df906ee
binarydevelop <binarydevelop@gmail.com>
2021-04-27 11:12:04 +0530

    add watch mode to jest.
    added a script  in package.json to start jest in wath mode

--- functions.js
+++ functions.js
@@ -1,7 +1,26 @@
 const functions = {
     add: (num1 , num2) => {
         return num1 + num2;
-    } 
+    },
+
+    isNull: () => {
+        return null; 
+    },
+
+    checkValue: (x) => {
+        return x
+    },
+
+    createUser: () => {
+        const user = { firstName: 'Tushar', lastName: 'Roy'};
+        return user;
+    },
+
+     myBeverage : {
+        delicious: true,
+        sour: false,
+      }
+
 }
 
-module.exports = functions;
+module.exports = functions; 
